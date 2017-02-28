e232646f299bfc891cccda78e152f1546c46b424
John J Czaplewski <john@czaplewski.org>
2017-02-28 16:38:11 -0600

    Use Mapbox static maps api for checkin map. Closes #15

--- web-app/src/services/settings.service.ts
+++ web-app/src/services/settings.service.ts
@@ -3,4 +3,5 @@ export class Settings {
   public static get PBDBURL(): string { return 'https://paleobiodb.org/data1.2' }
   public static get MACROSTRATURL(): string { return 'https://dev.macrostrat.org/api/v2' }
   public static get GEODEEPDIVEURL(): string { return 'https://geodeepdive.org/api/v1' }
+  public static get MAPBOXAPIKEY(): string { return 'pk.eyJ1IjoiamN6YXBsZXdza2kiLCJhIjoiWnQxSC01USJ9.oleZzfREJUKAK1TMeCD0bg' }
 }
