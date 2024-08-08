eb1ab8f0f2fdbe54b7331752bc195fa509f6e105
davidsklar99 <davidsklar99@outlook.com>
2024-08-08 12:44:46 -0400

    finished footer ts

--- pages/dev/test-site/index.ts
+++ pages/dev/test-site/index.ts
@@ -20,30 +20,23 @@ export function Navbar() {
 
 export function Footer() {
     return h("div", {className: "footer"}, [
-        h("div", {className: "footer-container"}, [
-            h("div", {className: "col-sm-4"}, [
-                h(Image, {className: "logo_white", src: "logo_white.png", width: "100px"}),
-                h("p", {className: "f1-text"}, [
-                    "Produced by the ",
-                    h("a", {href: "http://strata.geology.wisc.edu", target: "_blank"}, "UW Macrostrat Lab"),
-                    h("a", {href: "https://github.com/UW-Macrostrat", target: "_blank"}, h(Image, {className: "git_logo", src: "git-logo.png", width: "18px"})),
-                ])
+        h("div", {className: "titles"}, [
+            h("h3", {className: "footer-text"}, [
+                "Produced by the ",
+                h("a", {href: "https://macrostrat.org"} , "UW Macrostrat Lab")
             ]),
-            h("div", {className: "col-sm-4"}, [
-                h("ul", {className: "footer-nav"}, [
-                    h("li", h("a", {href: "/dev/test-site/about"}, "About")),
-                    h("li", h("a", {href: "/dev/test-site/publications"}, "Publications")),
-                    h("li", h("a", {href: "/dev/test-site/people"}, "People")),
-                    h("li", h("a", {href: "/dev/test-site/donate"}, "Donate"))
-                ])
-            ]),
-            h("div", {className: "col-sm-4"}, [
-                h(Image, {className: "funding-logo", src: "nsf.png", width: "100px"}),
-                h("div", {className: "funding-line"}, "Current support:"),
-                h("div", {className: "funding-line"}, "EAR-1948843"),
-                h("div", {className: "funding-line"}, "ICER-1928323"),
-                h("div", {className: "funding-line"}, "UW-Madison Dept. Geoscience")
+            h("h3", {className: "footer-text"}, [
+                "Funded by ",
+                h("a", {href: "https://nsf.gov"}, "NSF"),
+                " and ",
+                h("a", {href: "http://geoscience.wisc.edu/geoscience/"}, "UW Geoscience")
             ])
+        ]),
+        h("ul", {className: "footer-links"},[
+            h("li", h("a", {href: "/dev/test-site/main-page"}, "Home")),
+            h("li", h("a", {href: "/dev/test-site/explore"}, "Explore")),
+            h("li", h("a", {href: "/dev/test-site/privacy"}, "Privacy Policy")),
+            h("li", h("a", {href: "/dev/test-site/terms_and_conditions"}, "Terms and Conditions"))
         ])
     ]);
 }
