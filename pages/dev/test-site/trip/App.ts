88bbe9bfccaa9e25b999d5c0bbe0e64d65e0462b
davidsklar99 <davidsklar99@outlook.com>
2024-08-16 10:52:59 -0600

    fetchign trip data

--- pages/dev/test-site/trip/App.ts
+++ pages/dev/test-site/trip/App.ts
@@ -10,29 +10,42 @@ function getTrip() {
 
 export function App() {
     const trip = getTrip();
+    let data  = ''
 
-    // change conidition to match total number of trips
-    if(trip < 100) {
-        console.log("Trip " + trip + " found");
-        return h("div", { className: 'error'}, [
-            h("div", [
-                h("h1", "Trip " + String(trip) + " found")
-            ])
-        ]);
-    } else {
-        console.log("Trip " + trip + " not found");
-        return h("div", { className: 'error'}, [
-            h("div", [
-                h("h1", "Trip " + String(trip) + " not found")
-            ])
-        ]);
-    }
+    fetch("https://rockd.org/api/v2/trips/" + trip)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(myJson) {
+        data = myJson;
+
+        // change conidition to match total number of trips
+        if(trip < 100) {
+            console.log("Trip " + trip + " found");
+            console.log(data["first_name"] + " " + data['last_name'] + " took a trip to " + data["name"]);
+            return h("div", { className: 'error'}, [
+                h("div", [
+                    h("h1", "Trip " + String(trip) + " found"),
+                    h("p", data["first_name"] + " " + data['last_name'] + " took a trip to " + data["name"]),
+                ])
+            ]);
+        } else {
+            console.log("Trip " + trip + " not found");
+            console.log(data["first_name"] + " " + data['last_name'] + " took a trip to " + data["name"]);
+            return h("div", { className: 'error'}, [
+                h("div", [
+                    h("h1", "Trip " + String(trip) + " not found"),
+                    h("p", data["first_name"] + " " + data['last_name'] + " took a trip to " + data["name"]),
+                ])
+            ]);
+        }
+    });
 }
 
 export function TripSearch() {
     const trip = searchClick();
     var searchBtn = h("a", {  className: "search-btn", type: "button", href: "/dev/test-site/trip?trip=3" }, "Search");
-    
+
     return h(
         "div", { className: "trip-search" },[
             h("h1", { className: "trip-q" }, "Trip Search:"),
