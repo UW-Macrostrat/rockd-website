180130f4d858234709d283bfeff7e1127088b9a6
davidsklar99 <davidsklar99@outlook.com>
2024-09-11 16:28:00 -0500

    added sample trip data

--- pages/dev/test-site/trip/Map.ts
+++ pages/dev/test-site/trip/Map.ts
@@ -3,6 +3,7 @@ import h from "@macrostrat/hyper";
 import React, { useEffect, useRef } from 'react';
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
+import { Image } from "../index";
 
 export function Map() {
     const mapContainerRef = useRef(null);
@@ -10,7 +11,7 @@ export function Map() {
     useEffect(() => {
         if (mapContainerRef.current) {
             // update getting access key
-            mapboxgl.accessToken = "fill-in";
+            mapboxgl.accessToken = "XXXX";
 
             new mapboxgl.Map({
                 container: mapContainerRef.current,
@@ -25,9 +26,17 @@ export function Map() {
             h("div", { ref: mapContainerRef, className: 'map-container', style: { width: '100%', height: '80vh' } }),
             h('div', { className: 'stop-container', style: { width: '100%', height: '80vh' } }, [
                 h('div', { className: 'stop-header' }, [
-                    h('h3', {className: 'name'}, "Person Name"),
-                    h('h1', {className: 'park'}, "Park Name"),
-                ])
+                    h('h3', {className: 'profile-pic'}, h("Image", {src:"sample.jpg"})),
+                    h('div', {className: 'stop-main-info'}, [
+                        h('h3', {className: 'name'}, "Stop Name"),
+                        h('h3', {className: 'edited'}, "Edited: 1/1/2021"),
+                    ]),
+                ]),
+                h('h1', {className: 'park'}, "Park Name"),
+                h('div', {className: 'stop-description'}, [
+                    h('h2', {className: 'stop-title'}, "Stop Title"),
+                    h('p', {className: 'stop-text'}, "Stop Description"),
+                ]),
             ])
         ]);
 }
