634027fa0201b5f55d4865273129c11509b2c38e
davidsklar99 <davidsklar99@outlook.com>
2024-10-07 20:29:02 -0500

    deleted some more

--- pages/_simple_test/+Page.tsx
+++
@@ -1,6 +0,0 @@
-import styles from "./style.module.css";
-import React from "react";
-
-export function Page() {
-  return <div className={styles.test}>Hello world</div>;
-}
--- pages/map/map-interface/map-page/map-styles/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./map-sources";
-export * from "./line-symbols";
-export * from "./paleogeography";
-export * from "./overlay";
