b1f1e624bdcef53acb60d3d4cc442ce3f6c327f3
davidsklar99 <davidsklar99@outlook.com>
2025-03-31 15:45:03 -0500

    state control checkin

--- pages/checkin/index.ts
+++ pages/checkin/index.ts
@@ -20,8 +20,6 @@ export function Checkins({checkinID}) {
     const [overlayImage, setOverlayImage] = useState(null);
     const [overlayBody, setOverlayBody] = useState(null);
     const [showMap, setShowMap] = useState(false);
-    const [style, setStyle] = useState("mapbox://styles/jczaplewski/cje04mr9l3mo82spihpralr4i");
-    const [styleText, setStyleText] = useState("Show Satelite");
 
     if (!checkinData) {
         return h("div", { className: 'loading' }, [
@@ -95,41 +93,6 @@ export function Checkins({checkinID}) {
         }        
     });
 
-    const newMapPosition: MapPosition = {
-        camera: {
-          lat: center.lat,  // Latitude
-          lng: center.lng, // Longitude
-          altitude: 300000, // Altitude (height from the Earth's surface)
-        },
-      };
-
-    const sateliteStyle = 'mapbox://styles/mapbox/satellite-v9';
-    const whiteStyle = "mapbox://styles/jczaplewski/cje04mr9l3mo82spihpralr4i";
-    const whiteText = "Show White";
-    const sateliteText = "Show Satelite";
-
-    let map = h("div.map", [
-        h(MapAreaContainer, { style: {height: "93vh", top: "7vh"} },
-            [
-              h(MapView, { style: style, mapboxToken: SETTINGS.mapboxAccessToken, mapPosition: newMapPosition }, [
-                h(MapMarker, {
-                    position: center,
-                   }),
-              ]),
-            ]
-          ),
-        h('div', {className: 'banner'}, [
-            h(Icon, {className: "left-arrow banner-arrow", icon: "arrow-left", iconSize: "4vh", style: {color: 'black'}, onClick: () => {
-                setShowMap(!showMap);
-              }}),
-            h(PanelCard, {className: "banner-button", onClick: () => {
-                console.log("clicked");
-                setStyle(style == whiteStyle ? sateliteStyle : whiteStyle);
-                setStyleText(styleText == whiteText ? sateliteText : whiteText);
-            }}, styleText),
-        ]),
-    ])
-
     let LngLatProps = {
         position: {
             lat: checkin.lat,
@@ -153,6 +116,8 @@ export function Checkins({checkinID}) {
             overlayBody,
         ])
     ]);
+
+    const map = h(Map, {center, showMap, setShowMap});
 
     let main = h('div', { className: "container" }, [
         h('div', { className: showMap ? 'hide' : 'main'}, [
@@ -220,4 +185,46 @@ function observationFooter(observation) {
             h('p', {className: "notes"}, rocks.notes),
         ]),
     ]);
+}
+
+function Map({center, showMap, setShowMap}) {
+    const [style, setStyle] = useState("mapbox://styles/jczaplewski/cje04mr9l3mo82spihpralr4i");
+    const [styleText, setStyleText] = useState("Show Satelite");
+
+    const newMapPosition: MapPosition = {
+        camera: {
+          lat: center.lat,  // Latitude
+          lng: center.lng, // Longitude
+          altitude: 300000, // Altitude (height from the Earth's surface)
+        },
+      };
+
+    const sateliteStyle = 'mapbox://styles/mapbox/satellite-v9';
+    const whiteStyle = "mapbox://styles/jczaplewski/cje04mr9l3mo82spihpralr4i";
+    const whiteText = "Show White";
+    const sateliteText = "Show Satelite";
+
+    let map = h("div.map", [
+        h(MapAreaContainer, { style: {height: "93vh", top: "7vh"} },
+            [
+              h(MapView, { style: style, mapboxToken: SETTINGS.mapboxAccessToken, mapPosition: newMapPosition }, [
+                h(MapMarker, {
+                    position: center,
+                   }),
+              ]),
+            ]
+          ),
+        h('div', {className: 'banner'}, [
+            h(Icon, {className: "left-arrow banner-arrow", icon: "arrow-left", iconSize: "4vh", style: {color: 'black'}, onClick: () => {
+                setShowMap(!showMap);
+              }}),
+            h(PanelCard, {className: "banner-button", onClick: () => {
+                console.log("clicked");
+                setStyle(style == whiteStyle ? sateliteStyle : whiteStyle);
+                setStyleText(styleText == whiteText ? sateliteText : whiteText);
+            }}, styleText),
+        ]),
+    ])
+
+    return map;
 }
