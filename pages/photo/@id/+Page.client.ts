0fcd8d3c62ed2ad0c19fee68f77d32d1e638a070
davidsklar99 <davidsklar99@outlook.com>
2025-04-22 11:29:55 -0500

    photo carousel start

---
+++ pages/photo/@id/+Page.client.ts
@@ -0,0 +1,9 @@
+import h from "@macrostrat/hyper";
+import { usePageContext } from 'vike-react/usePageContext';
+import { Photos } from "../index"; 
+
+export function Page() {
+    const pageContext = usePageContext();
+    const photoID = parseInt(pageContext.urlParsed.pathname.split("/")[2]);
+    return h(Photos, { photoID });
+}
