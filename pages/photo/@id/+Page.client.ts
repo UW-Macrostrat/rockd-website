bf6f7c84be47aa95f66b2ef51d19b0271c43c1c1
davidsklar99 <davidsklar99@outlook.com>
2025-04-22 13:09:15 -0500

    photo changes

--- pages/photo/@id/+Page.client.ts
+++ pages/photo/@id/+Page.client.ts
@@ -1,9 +1,13 @@
 import h from "@macrostrat/hyper";
 import { usePageContext } from 'vike-react/usePageContext';
 import { Photos } from "../index"; 
+import { BlankImage, imageExists, Footer, apiURL, apiURLOld, useRockdAPI } from "../../index";
+
 
 export function Page() {
     const pageContext = usePageContext();
     const photoID = parseInt(pageContext.urlParsed.pathname.split("/")[2]);
-    return h(Photos, { photoID });
+    const checkinData = useRockdAPI("protected/checkins?photo_id=" + photoID);
+
+    return h(Photos, { photoID, checkinData });
 }
