ee67047c955b18c26f01e39e6a852e2482cd9260
davidsklar99 <davidsklar99@outlook.com>
2025-06-11 17:18:55 -0500

    checkin data handler

--- pages/checkin/@id/+data.ts
+++ pages/checkin/@id/+data.ts
@@ -3,13 +3,10 @@ import { rockdApiURL } from "@macrostrat-web/settings";
 
 export async function data(pageContext) {
     const checkin = await fetch(
-        `${rockdApiURL}/checkins?checkin_id=${pageContext.routeParams.id}`
+        `${rockdApiURL}/protected/checkins?checkin_id=${pageContext.routeParams.id}`
     ).then((response) => {
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        return response.json()[0];
+        return response.json();
     });
 
-    return { checkin }
+    return { checkin: checkin.success.data[0] };
 }
