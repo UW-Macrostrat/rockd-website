fc342193ce9e893e1e821c3fb1dd91bce3e09a15
davidsklar99 <davidsklar99@outlook.com>
2025-07-09 10:01:17 -0500

    Fix footer

--- src/components/general/index.ts
+++ src/components/general/index.ts
@@ -13,11 +13,10 @@ import { useState } from "react";
 import { navigate } from "vike/client/router";
 
 export function Footer() {
-  const isDarkMode = useDarkMode().isEnabled;
   const footerLinks1 = [
     { href: "/", icon: "home", text: "Home" },
     { href: "/explore", icon: "geosearch", text: "Explore" },
-    { href: "/trip/1", icon: "route", text: "Trip" },
+    { href: "/heatmap", icon: "map", text: "Heatmap" },
   ];
 
   const footerLinks2 = [
@@ -44,76 +43,8 @@ export function Footer() {
       ]),
     ]),
     h("div", { className: "footer-links" }, [
-      h("ul", [
-        h(
-          "li",
-          h("a", { href: "/" }, [
-            h(Icon, {
-              className: "footer-icon" + (isDarkMode ? "icon-dark-mode" : ""),
-              icon: "home",
-              style: { color: "white" },
-            }),
-            h("p", "Home"),
-          ])
-        ),
-        h(
-          "li",
-          h("a", { href: "/explore" }, [
-            h(Icon, {
-              className: "footer-icon" + (isDarkMode ? "icon-dark-mode" : ""),
-              icon: "geosearch",
-              style: { color: "white" },
-            }),
-            h("p", "Explore"),
-          ])
-        ),
-        h(
-          "li",
-          h("a", { href: "/heatmap" }, [
-            h(Icon, {
-              className: "footer-icon" + (isDarkMode ? "icon-dark-mode" : ""),
-              icon: "map",
-              style: { color: "white" },
-            }),
-            h("p", "Heatmap"),
-          ])
-        ),
-      ]),
-      h("ul", [
-        h(
-          "li",
-          h("a", { href: "/metrics" }, [
-            h(Icon, {
-              className: "footer-icon" + (isDarkMode ? "icon-dark-mode" : ""),
-              icon: "chart",
-              style: { color: "white" },
-            }),
-            h("p", "Metrics"),
-          ])
-        ),
-        h(
-          "li",
-          h("a", { href: "/terms" }, [
-            h(Icon, {
-              className: "footer-icon" + (isDarkMode ? "icon-dark-mode" : ""),
-              icon: "manual",
-              style: { color: "white" },
-            }),
-            h("p", "Terms and Conditions"),
-          ])
-        ),
-        h(
-          "li",
-          h("a", { href: "/privacy" }, [
-            h(Icon, {
-              className: "footer-icon" + (isDarkMode ? "icon-dark-mode" : ""),
-              icon: "lock",
-              style: { color: "white" },
-            }),
-            h("p", "Privacy"),
-          ])
-        ),
-      ]),
+      h("ul", footerLinks1.map((props) => h(FooterLink, props))),
+      h("ul", footerLinks2.map((props) => h(FooterLink, props))),
     ]),
     h("div.dark-mode", [
       h(DarkModeButton, { className: "dark-mode-button", showText: true }),
